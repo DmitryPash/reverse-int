c8d0c21b0b0f98a7ba4cf76f0f142904f51fe1c6
Dmitry <fomnq9@gmail.com>
2023-08-03 21:04:54 +0300

    init: start reverse-int
    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let mn;
+    if(n < 0) {
+       mn = -n
+    } else {
+       mn = n
+    }
+  let sn = String(mn);
+  let nm = sn.split('').reverse().join('')
+  return Number(nm)
 }
